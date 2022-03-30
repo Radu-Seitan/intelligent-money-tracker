b5c374fd8558da602d970401a7d0086ed74f444a
Radu-Seitan <raducseitan@gmail.com>
2022-03-30 13:39:29 +0300

    Clean Architecture Setup

---
+++ IMT-Backend/IMT-Backend.Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace IMT_Backend.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public BaseEntity()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}
