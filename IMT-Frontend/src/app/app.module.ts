ad8a40f40abfdd0f229877cf2b8afd24d75e85b6
Nicoleta Panaghiu <idlnicoleta@gmail.com>
2022-05-07 15:50:49 +0300

    IMT-16: Connected the angular application to firebase

--- IMT-Frontend/src/app/app.module.ts
+++ IMT-Frontend/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from "@angular/fire/compat";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
