3516f32642d3ca16faa625867c98b18525239c4b
Deea <hanes.andreea@yahoo.com>
2022-05-08 13:14:51 +0300

    IMT-20 Sign Up Page

--- IMT-Frontend/src/app/app.module.ts
+++ IMT-Frontend/src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    NbThemeModule,
+    NbLayoutModule,
+    NbSidebarModule,
+    NbButtonModule,
+    NbMenuModule,
+    NbSidebarService, NbIconModule, NbFormFieldModule, NbInputModule, NbStepperModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NbMenuModule.forRoot(),
+        NbThemeModule.forRoot({name: 'dark'}),
+        NbLayoutModule,
+        NbEvaIconsModule,
+        RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
+        NbLayoutModule,
+        NbSidebarModule, // NbSidebarModule.forRoot(), //if this is your app.module
+        NbButtonModule,
+        NbIconModule,
+        NbFormFieldModule,
+        NbInputModule,
+        NbStepperModule,
+    ],
+  providers: [NbSidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
