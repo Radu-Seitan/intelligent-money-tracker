4ef253922dac80d1f7ed24608ec49e618f32d10d
Radu-Seitan <raducseitan@gmail.com>
2022-05-12 13:03:25 +0300

    Added automatic db migrations

---
+++ IMT-Backend/IMT-Backend.WebUI/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,15 @@
+﻿using IMT_Backend.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMT_Backend.WebUI.Extensions
+{
+    public static class InfrastructureExtensions
+    {
+        public static void MigrateDatabase(this WebApplication webApplication)
+        {
+            using var scope = webApplication.Services.CreateScope();
+
+            scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
+        }
+    }
+}
