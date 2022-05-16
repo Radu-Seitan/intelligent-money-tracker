80648cc76e9dff7bb02075c0fd29f562227f8c1c
Nicoleta Panaghiu <idlnicoleta@gmail.com>
2022-05-16 21:41:38 +0300

    IMT-19: Connected users to backend plus additional methods

--- IMT-Frontend/src/app/modules/auth/services/auth.service.ts
+++ IMT-Frontend/src/app/modules/auth/services/auth.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
 import {AuthData} from "../models/auth-data.model";
-import {GoogleAuthProvider} from 'firebase/auth'
 import {User} from "../../../models/user.model";
 import {UserService} from "../../../services/user.service";
 
@@ -10,21 +9,52 @@ import {UserService} from "../../../services/user.service";
 })
 export class AuthService {
 
+  currentUser: User | null = null;
+
   constructor(
     private _fireAuth: AngularFireAuth,
     private _userService: UserService,
   ) {
+    this.userSetup();
+  }
+
+  get isLoggedIn() {
+    return (localStorage.getItem('user') !== null && JSON.parse(localStorage.getItem('user')!) !== '{}')
+      && this.currentUser !== null;
   }
 
   loginWithEmail({email, password}: AuthData) {
-    return this._fireAuth.signInWithEmailAndPassword(email, password).then().catch(error => console.error(error));
-  }
-
-  loginWithGoogle() {
-    return this._fireAuth.signInWithPopup(new GoogleAuthProvider()).then().catch(error => console.error(error));
+    return this._fireAuth.signInWithEmailAndPassword(email, password).then(() => {
+      this.userSetup();
+    }).catch(error => console.error(error));
   }
 
   signUp(authData: AuthData, user: User) {
-    this._fireAuth.createUserWithEmailAndPassword(authData.email, authData.password).then().catch(error => console.error(error));
+    this._fireAuth.createUserWithEmailAndPassword(authData.email, authData.password).then((res) => {
+      user.id = res.user?.uid;
+      this._userService.createUser(user).subscribe()
+    }).catch(error => console.error(error));
+  }
+
+  signOut() {
+    return this._fireAuth.signOut().then(() => {
+      localStorage.setItem('user', '{}');
+      this.currentUser = null;
+    }).catch(error => console.log(error))
+  }
+
+  private userSetup() {
+    this._fireAuth.authState.subscribe(user => {
+      if (user) {
+        localStorage.setItem('user', JSON.stringify(user));
+        JSON.parse(localStorage.getItem('user')!);
+        console.log(user.uid)
+        this._userService.getUserById(user.uid).subscribe(data => this.currentUser = data);
+      } else {
+        localStorage.setItem('user', '{}');
+        JSON.parse(localStorage.getItem('user')!);
+        this.currentUser = null;
+      }
+    })
   }
 }
