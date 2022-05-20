edd624d9a86030bac0e406260fe7ce07ca7f9cab
Nicoleta Panaghiu <idlnicoleta@gmail.com>
2022-05-20 19:49:10 +0300

    - feat: implemented the image service

---
+++ IMT-Frontend/src/app/utils/file/image.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+  readonly #baseUrl = environment.apiUrl;
+  readonly #endpoint = '/api/Images';
+
+
+  constructor(private _http: HttpClient) { }
+
+  getImageByStoreId(storeId: string) {
+    return this._http.get(`${this.#baseUrl}${this.#endpoint}/${storeId}`, { responseType: 'blob' });
+  }
+}
