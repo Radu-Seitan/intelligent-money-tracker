611767db0f82e93c382671a545bb4679e700b2bf
Radu-Seitan <raducseitan@gmail.com>
2022-05-31 14:20:16 +0300

    Fix for deploy

---
+++ IMT-Frontend/src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const API_KEY = "AIzaSyBjb7kPjScU1HS28rwrurE4DyUETau5R5g";
+export const AUTH_DOMAIN = "intelligent-money-tracker.firebaseapp.com";
+export const PROJECT_ID = "intelligent-money-tracker";
+export const STORAGE_BUCKET = "intelligent-money-tracker.appspot.com";
+export const MESSAGING_SENDER_ID = "646486076642";
+export const APP_ID = "1:646486076642:web:b9be08788b60c5949b02bb";
